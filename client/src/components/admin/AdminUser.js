82d88dff7f203aa2a2e37057e1d9c3d4f6f0b5fa
zaratush <martinc.1@hotmail.com>
2021-05-17 05:02:13 -0300

    ok

--- client/src/components/admin/AdminUser.js
+++ client/src/components/admin/AdminUser.js
@@ -11,7 +11,7 @@ export const AdminUser = () => {
             access: "Admin"
         }
 
-        axios.post("http://localhost:3001/users/register/",product).then((res) => { console.log(res)}).catch(err => { console.log(err) })
+        axios.post("/users/register/",product).then((res) => { console.log(res)}).catch(err => { console.log(err) })
    
     }
     
