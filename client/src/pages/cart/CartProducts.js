854dc61242b673eed143f27d2a34714d3ec777fd
zaratush <martinc.1@hotmail.com>
2021-05-19 01:01:16 -0300

    avatar

--- client/src/pages/cart/CartProducts.js
+++ client/src/pages/cart/CartProducts.js
@@ -12,6 +12,7 @@ import IconButton from '@material-ui/core/IconButton';
 import RemoveShoppingCartOutlinedIcon from '@material-ui/icons/RemoveShoppingCartOutlined';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import { Link, useHistory } from 'react-router-dom'
+import Swal from 'sweetalert2' 
 
 export default function Cart() {
   
@@ -30,10 +31,10 @@ export default function Cart() {
   const handlerClick = () => {
     try {
       
+      dispatch(getUsersByEmail(currentUser.email))
     } catch (error) {
-      
+      Swal("Debe estar logueado")
     }
-    dispatch(getUsersByEmail(currentUser.email))
    
   }
 
