d42cd1f0216e1901925073df47a061b57a16fe49
luciano <lucianoojeda36@gmail.com>
2021-05-19 19:03:51 -0300

    banner

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import AppBar from '../components/appBar/AppBar'
 import Catalog from '../components/Product/catalog/Catalog';
 import CategoryCatalog from '../components/category/CatalogCategory'
+import Banner from '../components/banner/BannerImage';
 
 
 export const Home = (props) => {
     return (
         <div>
             <AppBar />
+            <Banner/>
             <div>
                 <CategoryCatalog />
                 <Catalog />
