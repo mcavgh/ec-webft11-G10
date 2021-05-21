dc9671684baaf3863b71e02daf27836ec4f12d58
zaratush <martinc.1@hotmail.com>
2021-05-21 20:29:49 -0300

    ok

--- client/src/components/appBar/AppBar.js
+++ client/src/components/appBar/AppBar.js
@@ -45,243 +45,196 @@ export default function PersistentDrawerLeft() {
   const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
-    if (currentUser) {
-      dispatch(getUsersByEmailId(currentUser.email));
-      //dispatch(getProductInCart)
-    }
-    const handleChange = (event) => {
-        setAuth(event.target.checked);
-    };
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-        if (!currentUser) {
-            history.push("/login")
-        }
 
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    }; return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar >
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Button onClick={refreshSearch} variant="h6" color='inherit' to="/" component={Link}>
-                        eatx
-                    </Button>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <SearchBar />
-                    </div>
-                    <div className={classes.grow} />
-                    <div >
-                        <IconButton to="/cart" component={Link}
-                            aria-label="" color="inherit">
 
-                            <Badge badgeContent={cartQuantity} color="secondary">
-                                <ShoppingCartIcon fontSize="large" />
-                            </Badge>
     dispatch(getQuantity());
   }, []);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
-  // const history=useHistory()
-  //USER BUTTON
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const dropdown = Boolean(anchorEl);
-
-  const refreshSearch = () => {
-    axios.get(`/products/`).then((result) => {
-      dispatch(searchProductSuccess(result.data));
-    });
-  };
+ 
   const handleChange = (event) => {
     setAuth(event.target.checked);
   };
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
     if (!currentUser) {
-      history.push("/login");
+      history.push("/login")
     }
   };
   const handleClose = () => {
     setAnchorEl(null);
-  };
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Button
-            onClick={refreshSearch}
-            variant="h6"
-            color="inherit"
-            to="/"
-            component={Link}
-          >
-            eatx
-          </Button>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <SearchBar />
-          </div>
-          <div className={classes.grow} />
-          <div>
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
+
+    // const history=useHistory()
+    //USER BUTTON
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const dropdown = Boolean(anchorEl);
+
+    const refreshSearch = () => {
+      axios.get(`/products/`).then((result) => {
+        dispatch(searchProductSuccess(result.data));
+      });
+    };
+    const handleChange = (event) => {
+      setAuth(event.target.checked);
+    };
+    const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+      if (!currentUser) {
+        history.push("/login");
+      }
+    };
+
+    return (  
+
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar >
             <IconButton
-              to="/cart"
-              component={Link}
-              aria-label=""
               color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, open && classes.hide)}
             >
-              <Badge badgeContent={cartQuantity} color="secondary">
-                <ShoppingCartIcon fontSize="large" />
-              </Badge>
+              <MenuIcon />
             </IconButton>
-          </div>
-          <div>
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              {currentUser ? (
-                <Avatar alt="Remy Sharp" src={currentUser.photoURL} />
+            <Button onClick={refreshSearch} variant="h6" color='inherit' to="/" component={Link}>
+              eatx
+                    </Button>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <SearchBar />
+            </div>
+            <div className={classes.grow} />
+            <div >
+              <IconButton to="/cart" component={Link}
+                aria-label="" color="inherit">
+
+                <Badge badgeContent={cartQuantity} color="secondary">
+                  <ShoppingCartIcon fontSize="large" />
+                </Badge>
+              </IconButton>
+            </div>
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                {currentUser ? (
+                  <Avatar alt="Remy Sharp" src={currentUser.photoURL} />
+                ) : (
+                  <AccountCircleIcon fontSize="large" />
+                )}
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={dropdown}
+                onClose={handleClose}
+              >
+                <div>
+                  <MenuItem onClick={handleClose}>Perfil</MenuItem>
+                  <MenuItem
+                    onClick={() =>
+                      app
+                        .auth()
+                        .signOut()
+                        .then((res) => handleClose())
+                    }
+                  >
+                    Log out
+                </MenuItem>
+                </div>
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
               ) : (
-                <AccountCircleIcon fontSize="large" />
+                <ChevronRightIcon />
               )}
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={dropdown}
-              onClose={handleClose}
-            >
-              <div>
-                <MenuItem onClick={handleClose}>Perfil</MenuItem>
-                <MenuItem
-                  onClick={() =>
-                    app
-                      .auth()
-                      .signOut()
-                      .then((res) => handleClose())
-                  }
-                >
-                  Log out
-                </MenuItem>
-              </div>
-            </Menu>
           </div>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "ltr" ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          <ListItem button to="/adminProduct" component={Link}>
-            <ListItemText
-              className={classes.barOptions}
-              primary="Administrar Productos"
-            />
-          </ListItem>
-          <ListItem button to="/CreateProduct" component={Link}>
-            <ListItemText
-              className={classes.barOptions}
-              primary="Crear Productos"
-            />
-          </ListItem>
-          <ListItem button to="/adminCategories" component={Link}>
-            <ListItemText
-              className={classes.barOptions}
-              primary="Administrar Categorias"
-            />
-          </ListItem>
-          <ListItem button to="/creaCategories" component={Link}>
-            <ListItemText
-              className={classes.barOptions}
-              primary="Crear Categorias"
-            />
-          </ListItem>
-          <ListItem button to="/PageCheckoutOrders" component={Link}>
-            <ListItemText
-              className={classes.barOptions}
-              primary="Ver Ordenes"
-            />
-          </ListItem>
-        </List>
-        <Divider />
-      </Drawer>
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
-        <div className={classes.drawerHeader} />
-      </main>
-    </div>
-  );
-}
+          <Divider />
+          <List>
+            <ListItem button to="/adminProduct" component={Link}>
+              <ListItemText
+                className={classes.barOptions}
+                primary="Administrar Productos"
+              />
+            </ListItem>
+            <ListItem button to="/CreateProduct" component={Link}>
+              <ListItemText
+                className={classes.barOptions}
+                primary="Crear Productos"
+              />
+            </ListItem>
+            <ListItem button to="/adminCategories" component={Link}>
+              <ListItemText
+                className={classes.barOptions}
+                primary="Administrar Categorias"
+              />
+            </ListItem>
+            <ListItem button to="/creaCategories" component={Link}>
+              <ListItemText
+                className={classes.barOptions}
+                primary="Crear Categorias"
+              />
+            </ListItem>
+            <ListItem button to="/PageCheckoutOrders" component={Link}>
+              <ListItemText
+                className={classes.barOptions}
+                primary="Ver Ordenes"
+              />
+            </ListItem>
+          </List>
+          <Divider />
+        </Drawer>
+        <main
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <div className={classes.drawerHeader} />
+        </main>
+      </div>
+    );
+  }
