f6d2e7003a3c5b69f3c37469b019d6367b1c6df0
Esteban Cespedes <ces.esteban@gmail.com>
2021-05-27 01:18:32 -0300

    2warnings

--- client/src/components/admin/Deposits.js
+++ client/src/components/admin/Deposits.js
@@ -5,40 +5,27 @@ import Title from './Title';
 import { useSelector, useDispatch } from "react-redux";
 import {getAllOrders} from "../../store/order/order.action"
 
-
-
-function preventDefault(event) {
-  event.preventDefault();
-}
-
 const useStyles = makeStyles({
   depositContext: {
     flex: 1,
   },
 });
 
-
 export default function Deposits({amounts}) {
-
   const dispatch = useDispatch();
 
-  useEffect(() => {
-   
-     dispatch(getAllOrders())
-    
+  useEffect(() => {   
+     dispatch(getAllOrders())    
   }, [dispatch])
    
   const amount = useSelector(state => state.orderReducer?.orders)
   const amount2 = amount.length>0&&amount.filter(e=>e.state!=="cancelada")
   const deposits= amount2.length>0&&amount.reduce( (ac,e)=>ac+e.price,0)
-
-
   const event = new Date().toLocaleDateString();
   const date = event.toString()
-
   const classes = useStyles();
-  return (
-    
+
+  return (    
     <React.Fragment>
       <Title>Total Ventas</Title>
       <Typography component="p" variant="h4">
