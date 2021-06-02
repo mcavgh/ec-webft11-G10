49eff46c2febb195d15c1bf13c4d0ec047eebc44
NuLuciano <nunezluciano@hotmail.com>
2021-06-02 19:44:38 -0300

    User profile terminado

--- client/src/store/user/user.reducer.js
+++ client/src/store/user/user.reducer.js
@@ -1,4 +1,4 @@
-import {GET_USER_WISHLIST, POST_USER_ACCESS, POST_ADMIN, GET_USER_BYID, GET_ID_BYEMAIL, DELETE_USER, PUT_USER, GET_USERS } from './user.action';
+import {GET_USER_WISHLIST, POST_USER_ACCESS, POST_ADMIN, GET_USER_BYID, GET_ID_BYEMAIL, DELETE_USER, PUT_USER, GET_USERS, EDIT_AVATAR } from './user.action';
 
 const initialState = {
   users: [],
@@ -16,6 +16,7 @@ const userReducer = (state = initialState, action) => {
       }
     case POST_USER_ACCESS:
     case POST_ADMIN:
+    case EDIT_AVATAR:
     case GET_USER_BYID:
       return {
         ...state,
